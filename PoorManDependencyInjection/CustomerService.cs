da9bc6cc13f60a537a0f48afe92260598c8a97bf
Boris Modylevsky <Boris Modylevsky>
2016-08-23 23:02:22 +0300

    add poor man's conatiner

---
+++ PoorManDependencyInjection/CustomerService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoorManDependencyInjection
+{
+    public class CustomerService : ICustomerService
+    {
+        private ICustomerRepository m_Repository;
+        private ICustomerDTOMapper m_Mapper;
+
+        public CustomerService(
+            ICustomerRepository repository,
+            ICustomerDTOMapper mapper)
+        {
+            m_Repository = repository;
+            m_Mapper = mapper;
+        }
+    }
+
+    public class ICustomerDTOMapper
+    {
+    }
+
+    public class CustomerDtoMapper : ICustomerDTOMapper
+    {
+    }
+
+    public interface ICustomerService
+    {
+    }
+
+    public interface ICustomerRepository
+    {
+        void GetCustomer(int userId);
+    }
+
+    public class CustomerRepository : ICustomerRepository
+    {
+        public void GetCustomer(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ PoorManDependencyInjection/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoorManDependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerRepository = new CustomerRepository();
+            var customerDtoMapper = new CustomerDtoMapper();
+            var customerService = new CustomerService(customerRepository, customerDtoMapper);
+        }
+    }
+}
